2c13e33d79ea5c1e02d12787231628b0bb760d17
MohamedMRamadan <mohamedmanchinooz@gmail.com>
2023-08-27 15:39:25 +0300

    Update imports , delete files

--- src/features/cabins/CabinRow-v1.jsx
+++ src/features/cabins/CabinRow-v1.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { formatCurrency } from "../../utils/helpers";
 
 import CreateCabinForm from "./CreateCabinForm";
-import useDeleteCabin from "./UseDeleteCabin";
+import useDeleteCabin from "./useDeleteCabin";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import useCreateCabin from "./useCreateCabin";
 import EditCabin from "./EditCabin";
--- src/features/cabins/EditCabin.jsx
+++
@@ -1,9 +0,0 @@
-import { HiPencil } from "react-icons/hi2";
-import Modal from "../../ui/Modal";
-import CreateCabinForm from "./CreateCabinForm";
-
-function EditCabin({ cabinToEdit }) {
-  return <div></div>;
-}
-
-export default EditCabin;
